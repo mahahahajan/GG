9a8a99ccfb1f3a0c39e7c849f697b1018eeaeff0
mahahahajan <Tiklum@PulkitApple.local>
2015-08-10 11:50:06 -0700

    Hard Code Generator

--- js/generate2.js
+++ js/generate2.js
@@ -3,16 +3,46 @@ window.onload = function () {
 'Dogs', 'Cats', 'Insults', 'Comebacks', "Your Mom Jokes", "Puns", "Inspirational Quotes", "Sentences", "Songs"];
   var randomValue = myTopics[Math.floor(Math.random() * myTopics.length)];
  console.log(randomValue);
- var name = document.createElement("p");
+ var name = document.createElement("h1");
  name.innerHTML = randomValue;
  document.body.appendChild(name);
 
+ var generate = document.createElement("p");
+
+ document.body.appendChild(generate);
+
+ //do if statements
   document.getElementById('button').onclick = function (){
+
+    //Baby Names
     if(randomValue == "Baby Names"){
       var myNames = ['Alex', 'Jamie', 'Marvin', 'Hurricane', 'Barney', 'Ted', 'Lily', 'Robin', 'Marshall'];
       var randomName = myNames[Math.floor(Math.random() * myNames.length)];
      console.log(randomName);
-     name.innerHTML = randomName;
+     generate.innerHTML = randomName;
+    }
+    //Shows
+    if(randomValue == "Shows"){
+      var myShows = ['Psych', 'How I Met Your Mother', 'Mr.Robot', 'Silicon Valley', 'Arrow', 'Flash', 'Agents of SHIELD', 'Goldbergs', 'Blackish'];
+      var randomShow = myShows[Math.floor(Math.random() * myShows.length)];
+     console.log(randomShow);
+     generate.innerHTML = randomShow;
+    }
+    //Pranks
+    if(randomValue == "Pranks"){
+      var myPranks = ['Psych', 'How I Met Your Mother', 'Mr.Robot', 'Silicon Valley', 'Arrow', 'Flash', 'Agents of SHIELD', 'Goldbergs', 'Blackish'];
+      var randomPrank = myPranks[Math.floor(Math.random() * myPranks.length)];
+     console.log(randomShow);
+     generate.innerHTML = randomPrank;
+    }
+    //Ideas
+    {
+      if(randomValue == "Ideas"){
+        var myIdeas = ['USB Belt', 'Wrist Keyboard', 'Chain Mail Texter', 'Text Message Game', 'Online Chat', 'Game of speed'];
+        var randomIdea = myIdeas[Math.floor(Math.random() * myIdeas.length)];
+       console.log(randomShow);
+       generate.innerHTML = randomIdea;
+      }
     }
   }
 }
