ed010cbf028331eadf39430c0ca61668571348ee
marcosruiz1999 <marqitos9898@gmail.com>
2015-08-10 10:49:20 -0700

    did stuff

--- js/bg.js
+++ js/bg.js
@@ -1,16 +1,15 @@
 window.onload = function() {
-    
-
-function changeBackground() {
-    var color = getRandomColor();
-   document.body.style.background = color;
-}
-}
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+	function changeBackground() {
+	    var color = getRandomColor();
+	   document.body.style.background = color;
+	}
+	function getRandomColor() {
+	    var letters = '0123456789ABCDEF'.split('');
+	    var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * 16)];
+	    }
+	    return color;
+	}
+	changeBackground();
 }
